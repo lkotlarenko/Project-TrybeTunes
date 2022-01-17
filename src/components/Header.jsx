407d3ecbe013d794d869d4132928247adce7daeb
lkotlarenko <lkotlarenko@protonmail.com>
2022-01-17 17:30:56 -0300

    Req. 4 done :frog:

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import * as userAPI from '../services/userAPI';
 import Loading from './Loading';
 
@@ -31,10 +32,30 @@ class Header extends Component {
   }
 
   render() {
-    const { isLoading, user: { name } } = this.state;
+    const {
+      isLoading,
+      user: { name },
+    } = this.state;
 
     return (
       <header data-testid="header-component">
+        <Link className="link-search" to="/search" data-testid="link-to-search">
+          Search
+        </Link>
+        <Link
+          className="link-favorites"
+          to="/favorites"
+          data-testid="link-to-favorites"
+        >
+          Favorites
+        </Link>
+        <Link
+          className="link-profile"
+          to="/profile"
+          data-testid="link-to-profile"
+        >
+          Profile
+        </Link>
         {isLoading ? <Loading /> : null}
         <p data-testid="header-user-name">{name}</p>
       </header>
